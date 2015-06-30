1b6022550d54988abe7466a90926a85bb781c563
lwalter86 <lionel.walter@univ-poitiers.fr>
2015-06-30 10:21:17 +0200

    ameliorations syntaxiques

--- testsql4.py
+++ testsql4.py
@@ -1,127 +1,130 @@
+
+"""
+projet tuteuré 2014-2015
+"""
 # Importation des bibliotheque
 import RPi.GPIO as GPIO
 import time
 import nxppy
 import sqlite3
-import sys
 
 # Connexion a la base de donnee
-connection=sqlite3.connect('nfc2.db')
-curs=connection.cursor()
+CONNECTION = sqlite3.connect('nfc2.db')
+curs = CONNECTION.cursor()
 
 # Definition des ports en entre/sortie
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(4,GPIO.OUT)
-GPIO.setup(17,GPIO.OUT)
-GPIO.setup(18,GPIO.OUT)
-GPIO.setup(22,GPIO.IN)
-GPIO.setup(25,GPIO.IN)
+GPIO.setup(4, GPIO.OUT)
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
+GPIO.setup(22, GPIO.IN)
+GPIO.setup(25, GPIO.IN)
 
 # Debut du programme
-while True :
-	uid = nxppy.read_mifare()		# Lecture et insertion dans une variable du numero de la carte NCF presentee
-	
-	#Condition pour l'ajout d une carte
-	if uid == "04F1D561EE0280":		# Si la carte maitre est detectee
-		if (GPIO.input(22)==0):		# Si le bouton poussoir noir est appuye
-			print "Presente la carte a ajouter"
-			GPIO.output(17,GPIO.HIGH)	# Clignotement LED
-			time.sleep(0.4)
-			GPIO.output(17,GPIO.LOW)
-			time.sleep(0.4)
-			GPIO.output(17,GPIO.HIGH)
-			time.sleep(0.4)
-			GPIO.output(17,GPIO.LOW)
-			time.sleep(0.4)
-			GPIO.output(17,GPIO.HIGH)
-			time.sleep(0.4)
-			GPIO.output(17,GPIO.LOW)
-			time.sleep(0.4)	
-			GPIO.output(17,GPIO.HIGH)
-			time.sleep(0.4)
-			GPIO.output(17,GPIO.LOW)
-			time.sleep(0.4)			# Fin clignotement
+while True:
+    UID = nxppy.read_mifare()        # Lecture et insertion dans une variable du numero de la carte NCF presentee
 
-			ajou = nxppy.read_mifare()	# Lecture et insertion dans une variable de la carte NCF presentee
-			curs.execute('SELECT * FROM carte')	# Lecture de la base de donnee
-			listebdd = curs.fetchall()		# Insertion BDD dans la variable listebdd
+    #Condition pour l'ajout d une carte
+    if UID == "04F1D561EE0280":        # Si la carte maitre est detectee
+        if GPIO.input(22) == 0:        # Si le bouton poussoir noir est appuye
+            print "Presente la carte a ajouter"
+            GPIO.output(17, GPIO.HIGH)    # Clignotement LED
+            time.sleep(0.4)
+            GPIO.output(17, GPIO.LOW)
+            time.sleep(0.4)
+            GPIO.output(17, GPIO.HIGH)
+            time.sleep(0.4)
+            GPIO.output(17, GPIO.LOW)
+            time.sleep(0.4)
+            GPIO.output(17, GPIO.HIGH)
+            time.sleep(0.4)
+            GPIO.output(17, GPIO.LOW)
+            time.sleep(0.4)
+            GPIO.output(17, GPIO.HIGH)
+            time.sleep(0.4)
+            GPIO.output(17, GPIO.LOW)
+            time.sleep(0.4)            # Fin clignotement
 
-			if (str(ajou) in str(listebdd)):	# Si la carte est dans la BDD
-				print ('la carte ' + str(ajou) + ' est deja autorisee')
-				GPIO.output(18,GPIO.HIGH)	# Allumer LED rouge
+            AJOU = nxppy.read_mifare()    # Lecture et insertion dans une variable de la carte NCF presentee
+            curs.execute('SELECT * FROM carte')    # Lecture de la base de donnee
+            LISTEBDD = curs.fetchall()        # Insertion BDD dans la variable listebdd
 
-			elif (str(ajou)=="None"):		# Si pas de carte detectee
-				print "Aucune carte detectee"
-				GPIO.output(18,GPIO.HIGH)	# Allumer LED rouge
+            if str(AJOU) in str(LISTEBDD):    # Si la carte est dans la BDD
+                print 'la carte ' + str(AJOU) + ' est deja autorisee'
+                GPIO.output(18, GPIO.HIGH)    # Allumer LED rouge
 
-			else:
-				curs.execute("INSERT INTO carte (card) values (?)",(ajou,))	# Ajout dans la BDD
-				print ('la carte ' + str(ajou) + ' a ete ajoute')
-				connection.commit()		# Enregistrement des modifications dans la BDD
-				GPIO.output(17,GPIO.HIGH)	# Allumer LED verte
-				time.sleep(1)
-				GPIO.output(17,GPIO.LOW)	# Eteindre LED verte
-		time.sleep(1)
-		GPIO.output(18,GPIO.LOW)	# Eteindre LED rouge
+            elif str(AJOU) == "None":        # Si pas de carte detectee
+                print "Aucune carte detectee"
+                GPIO.output(18, GPIO.HIGH)    # Allumer LED rouge
+
+            else:
+                curs.execute("INSERT INTO carte (card) values (?)", (AJOU,))    # Ajout dans la BDD
+                print 'la carte ' + str(AJOU) + ' a ete ajoute'
+                CONNECTION.commit()        # Enregistrement des modifications dans la BDD
+                GPIO.output(17, GPIO.HIGH)    # Allumer LED verte
+                time.sleep(1)
+                GPIO.output(17, GPIO.LOW)    # Eteindre LED verte
+        time.sleep(1)
+        GPIO.output(18, GPIO.LOW)    # Eteindre LED rouge
 
 
-		#Condition pour la suppression d une carte
-		if GPIO.input(25)==0:		# Si le bouton poussoir rouge est appuye
-			print "Presente la carte a supprimer"	
-			GPIO.output(18,GPIO.HIGH)		# Clignotement LED rouge
-			time.sleep(0.4)
-			GPIO.output(18,GPIO.LOW)
-			time.sleep(0.4)
-			GPIO.output(18,GPIO.HIGH)
-			time.sleep(0.4)
-			GPIO.output(18,GPIO.LOW)
-			time.sleep(0.4)
-			GPIO.output(18,GPIO.HIGH)
-			time.sleep(0.4)
-			GPIO.output(18,GPIO.LOW)
-			time.sleep(0.4)	
-			GPIO.output(18,GPIO.HIGH)
-			time.sleep(0.4)
-			GPIO.output(18,GPIO.LOW)
-	 
-			supr = nxppy.read_mifare()		# Lecture et insertion dans une variable du numero de la carte NCF presentee
-			curs.execute('SELECT * FROM carte')	# Lecture de la base de donnee
-			bas = curs.fetchall()			# Insertion BDD dans la variable bas
+        #Condition pour la suppression d une carte
+        if GPIO.input(25) == 0:        # Si le bouton poussoir rouge est appuye
+            print "Presente la carte a supprimer"
+            GPIO.output(18, GPIO.HIGH)        # Clignotement LED rouge
+            time.sleep(0.4)
+            GPIO.output(18, GPIO.LOW)
+            time.sleep(0.4)
+            GPIO.output(18, GPIO.HIGH)
+            time.sleep(0.4)
+            GPIO.output(18, GPIO.LOW)
+            time.sleep(0.4)
+            GPIO.output(18, GPIO.HIGH)
+            time.sleep(0.4)
+            GPIO.output(18, GPIO.LOW)
+            time.sleep(0.4)
+            GPIO.output(18, GPIO.HIGH)
+            time.sleep(0.4)
+            GPIO.output(18, GPIO.LOW)
 
-			if supr == "04F1D561EE0280":		# Si on presente la carte maitre
-				print ("la carte maitre ne peut pas etre suprimmee")
-				GPIO.output(18,GPIO.HIGH)	# Allumer LED rouge
-				time.sleep(1)
-				GPIO.output(18,GPIO.LOW)	# Eteindre LED rouge
+            SUPR = nxppy.read_mifare()        # Lecture et insertion dans une variable du numero de la carte NCF presentee
+            curs.execute('SELECT * FROM carte')    # Lecture de la base de donnee
+            BAS = curs.fetchall()            # Insertion BDD dans la variable bas
 
-			elif str(supr) in str(bas): 		# Si la carte est dans la BDD
-				curs.execute("delete from carte where card=(?)",(supr,))	# Suppression dans la BDD
-				print ('la carte ' + str(supr) + ' a ete supprime')
-				connection.commit()		# Enregistrement des modifications dans la BDD
-				GPIO.output(17,GPIO.HIGH)	# Allumer LED verte
-				time.sleep(1)
-				GPIO.output(17,GPIO.LOW)	# Eteindre LED verte
-				
-			else : 
-				print("La carte n est pas dans la base de donnee")
-				GPIO.output(18,GPIO.HIGH)	# Allumer LED rouge
-				time.sleep(1)
-				GPIO.output(18,GPIO.LOW)	# Eteindre LED rouge
-			
+            if SUPR == "04F1D561EE0280":        # Si on presente la carte maitre
+                print "la carte maitre ne peut pas etre suprimmee"
+                GPIO.output(18, GPIO.HIGH)    # Allumer LED rouge
+                time.sleep(1)
+                GPIO.output(18, GPIO.LOW)    # Eteindre LED rouge
 
-	lect = nxppy.read_mifare()		# Lecture et insertion dans une variable du numero de la carte NCF presentee
-	if (GPIO.input(22)==1):			# Si aucun bouton poussoir n est presse
-		if (GPIO.input(25)==1):
-			curs.execute('SELECT * FROM carte')	# Lecture de la base de donnee
-			base = curs.fetchall()			# Insertion BDD dans la variable base
-			if str(lect) in str(base):		# Si la carte est dans la BDD
-				print "allumer, c'est ouvert"
-				GPIO.output(17,GPIO.HIGH)	# Allumer LED verte
-				GPIO.output(4,GPIO.HIGH)	# Declencher relais
-				time.sleep(5)
-				GPIO.output(17,GPIO.LOW)	# Eteindre LED verte
-				GPIO.output(4,GPIO.LOW)		# Arret du declenchement du relais
-				print "on ferme"
-				time.sleep(1)
-			
-connection.close()		# Fermeture de la connection avec la BDD
+            elif str(SUPR) in str(BAS):         # Si la carte est dans la BDD
+                curs.execute("delete from carte where card=(?)", (SUPR,))    # Suppression dans la BDD
+                print 'la carte ' + str(SUPR) + ' a ete supprime'
+                CONNECTION.commit()        # Enregistrement des modifications dans la BDD
+                GPIO.output(17, GPIO.HIGH)    # Allumer LED verte
+                time.sleep(1)
+                GPIO.output(17, GPIO.LOW)    # Eteindre LED verte
+
+            else:
+                print "La carte n est pas dans la base de donnee"
+                GPIO.output(18, GPIO.HIGH)    # Allumer LED rouge
+                time.sleep(1)
+                GPIO.output(18, GPIO.LOW)    # Eteindre LED rouge
+
+
+    LECT = nxppy.read_mifare()        # Lecture et insertion dans une variable du numero de la carte NCF presentee
+    if GPIO.input(22) == 1:            # Si aucun bouton poussoir n est presse
+        if GPIO.input(25) == 1:
+            curs.execute('SELECT * FROM carte')    # Lecture de la base de donnee
+            BASE = curs.fetchall()            # Insertion BDD dans la variable base
+            if str(LECT) in str(BASE):        # Si la carte est dans la BDD
+                print "allumer, c'est ouvert"
+                GPIO.output(17, GPIO.HIGH)    # Allumer LED verte
+                GPIO.output(4, GPIO.HIGH)    # Declencher relais
+                time.sleep(5)
+                GPIO.output(17, GPIO.LOW)    # Eteindre LED verte
+                GPIO.output(4, GPIO.LOW)        # Arret du declenchement du relais
+                print "on ferme"
+                time.sleep(1)
+
+CONNECTION.close()        # Fermeture de la connection avec la BDD
